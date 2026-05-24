405f60e68928955ceb781a0468e4f81eb902e4b1
quyetle1007-rgb <quyetle1007@gmail.com>
2026-05-24 21:30:07 +0700

    add algorithms

--- components/user_panel.py
+++ components/user_panel.py
@@ -9,7 +9,7 @@ from algorithms.bfs import find_path_bfs
 from algorithms.ucs import find_path_ucs
 from algorithms.dfs import find_path_dfs
 from components.map_view import MetroMapVisualizer, load_line_info_from_json
-
+from algorithms.Dijkstra import find_path_dijkstra
 
 class UserScreen(tk.Frame):
     def __init__(self, parent, state):
@@ -52,7 +52,7 @@ class UserScreen(tk.Frame):
         self.entry_end.pack(fill="x", padx=25, pady=(5, 15))
 
         tk.Label(left, text="THUẬT TOÁN TÌM KIẾM", bg=metro_ui.CLR_PANEL, fg=metro_ui.CLR_SUBTEXT, font=("Arial", 8, "bold")).pack(padx=25, anchor="w")
-        self.algo_combo = ttk.Combobox(left, values=["A*", "BFS", "UCS","DFS"], state="readonly", font=("Arial", 10))
+        self.algo_combo = ttk.Combobox(left, values=["A*", "BFS", "UCS","DFS","Dijkstra"], state="readonly", font=("Arial", 10))
         self.algo_combo.current(0)
         self.algo_combo.pack(fill="x", padx=25, pady=(5, 20))
 
@@ -115,6 +115,7 @@ class UserScreen(tk.Frame):
             if algo == "A*": result = find_path_astar(mg_use, sid_start, sid_end)
             elif algo == "BFS": result = find_path_bfs(mg_use, sid_start, sid_end)
             elif algo == "DFS": result = find_path_dfs(mg_use, sid_start, sid_end)
+            elif algo == "Dijkstra": result = find_path_dijkstra(mg_use, sid_start, sid_end)
             else: result = find_path_ucs(mg_use, sid_start, sid_end)
             duration_ms = (time.perf_counter() - t_start) * 1000
             self.after(0, lambda: self._display_result(result, duration_ms, algo))
